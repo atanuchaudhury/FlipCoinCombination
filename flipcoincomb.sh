773b4500b0cb3aebf5d3dfcf39cce3db5e2e7f5e
atanuchaudhury <atanuchaudhury1994@gmail.com>
2020-08-17 00:00:02 +0530

    percent of singlet combination determined

--- flipcoincomb.sh
+++ flipcoincomb.sh
@@ -1,10 +1,32 @@
-#! /bin/bash
+#! /bin/bash -x
+
+declare -A countArr
+echo Enter number of turn
+read numberOfTurn
+
+head=1;
+tail=0;
+countHead=0;
+countTail=0;
+for (( i=0; i<=numberofTurn; i++ ))
+do
 
 Flip=$((RANDOM%2))
 
-if [ $Flip -eq 1 ]
+if [ $Flip -eq $head ]
 then
+	countHead=$(( $countHead+1 ))
         echo Head
 else
-        echo Tail
+	 countTail=$(( $countTail+1 ))
+         echo Tail
 fi
+done
+
+countArr[((countHead))]=$countHead
+countArr[((countTail))]=$countTail
+
+echo "Number of head " ${countArr[countHead]}
+echo "Number of Tail " ${countArr[countTail]}
+percentageHead=$(($countHead*100/$numberOfTurn))
+percentageTail=$(($countTail*100/$numberOfTurn))
